586a52f4b3be2b4bd27f67e7d1591af0a605827b
Christoph M. Konrad <c.m.konrad@tudelft.nl>
2026-04-14 16:22:43 +0200

    Create a proper vizualisation of the balancing rider model: 2D projection of the 3D bicycle.

--- src/cyclistsocialforce/parameters.py
+++ src/cyclistsocialforce/parameters.py
@@ -11,7 +11,7 @@ import numpy as np
 import control as ct
 import warnings
 
-from cyclistsocialforce.utils import thresh
+from cyclistsocialforce.utils import thresh, dataunits_to_points
 from pypaperutils.design import TUDcolors
 
 from bicycleparameters.parameter_dicts import meijaard2007_browser_jason
@@ -175,6 +175,150 @@ class VehicleDrawingParameters:
             or self.draw_forces_repulsive
             or self.draw_forces_resulting
         )
+
+
+class BalancingRiderDrawingParameters(VehicleDrawingParameters):
+    """Class storing and maintaining the parameters for a balancing rider bicycle drawing.
+
+    Parameters include colors,
+    To be used together with cyclistsocialforce.visualisation.BalancingRiderDrawing
+
+    """
+
+    def __init__(
+        self,
+        bike, 
+        bike_color_frame=None,
+        bike_color_wheels=None,
+        rider_color_body=None,
+        rider_color_head=None,
+        bike_thickness_wheels=0.03,
+        bike_thickness_frame=0.05,
+        draw_frontwheel_trajectory=True,
+        proj_3d=False,
+        **kwargs,
+    ):
+        """Create a bicycle drawing parameters object.
+
+
+        Parameters
+        ----------
+        bike_color_frame : color, optional
+            The default is TU Delft cyan.
+        bike_color_wheels : color, optional
+            The default is gray.
+        rider_color_body : color or list of colors, optional
+            The default is random sampling from all TU Delft colors. If
+            a list of colors is provided the body color is randomly sampled
+            from this list.
+        rider_color_head : color, optional
+            The default is TU Delft cyan.
+        proj3d : TYPE, optional
+            Prepares color lists for the 3D drawing instead of 2D.
+            The default is False.
+
+        Returns
+        -------
+        None.
+
+        """
+        super().__init__(**kwargs)
+
+        self.bicycleParameterDict = bike.params.bp_params_set.parameters
+
+        self.proj_3d = proj_3d
+        self.draw_frontwheel_trajectory = draw_frontwheel_trajectory
+
+        self.ewidth_frame = bike_thickness_frame
+        self.ewidth_wheel = bike_thickness_wheels
+
+        self.init_riderbike_colors(
+            bike_color_frame,
+            bike_color_wheels,
+            rider_color_body,
+            rider_color_head,
+        )
+        self.make_colorlists_riderbike()
+
+
+    def init_riderbike_colors(
+        self,
+        bike_color_frame=None,
+        bike_color_wheels=None,
+        rider_color_body=None,
+        rider_color_head=None,
+    ):
+        """Initializes the face and edge colors for the bike-rider polygon
+        including the roll indicator.
+
+
+        Parameters
+        ----------
+        bike_color_frame : color, optional
+            The default is TU Delft cyan.
+        bike_color_wheels : color, optional
+            The default is gray.
+        rider_color_body : color or list of colors, optional
+            The default is random sampling from all TU Delft colors. If
+            a list of colors is provided the body color is randomly sampled
+            from this list.
+        rider_color_head : color, optional
+            The default is TU Delft cyan.
+
+        Returns
+        -------
+        None.
+
+        """
+
+        if bike_color_frame is None:
+            bike_color_frame = self.tud_colors.get("cyaan")
+        if bike_color_wheels is None:
+            bike_color_wheels = "gray"
+
+        if rider_color_body is None:
+            rider_color_body = self.tud_colors.get(
+                np.random.randint(0, len(self.tud_colors.colors))
+            )
+        elif isinstance(rider_color_body, list):
+            rider_color_body = rider_color_body[
+                np.random.randint(0, len(rider_color_body))
+            ]
+
+        if rider_color_head is None:
+            rider_color_head = self.tud_colors.get("cyaan")
+
+        self.bike_color_frame = bike_color_frame
+        self.bike_color_wheels = bike_color_wheels
+        self.rider_color_body = rider_color_body
+        self.rider_color_head = rider_color_head
+
+    def make_colorlists_riderbike(self):
+        """Create the list of colors for the rider+bike polygons/ellipses.
+
+        Returns
+        -------
+        None.
+
+        """
+
+        self.ecolors_riderbike_poly = [
+            self.bike_color_frame,
+            self.bike_color_frame,
+            #self.rider_color_body,
+            #self.rider_color_body,
+            #self.rider_color_body,
+            #self.rider_color_head,
+        ]
+        self.fcolors_riderbike_poly = ["none"] * 2
+        self.ewidths_riderbike_poly = [self.ewidth_frame, self.ewidth_frame]
+
+        self.ecolors_riderbike_elli = [
+            self.bike_color_wheels,
+            self.bike_color_wheels,
+        ]
+        self.fcolors_riderbike_elli = ["none"] * 2
+        self.ewidths_riderbike_elli = [self.ewidth_wheel, self.ewidth_wheel]
 
 
 class BikeDrawing2DParameters(VehicleDrawingParameters):
---
+++ tests/test_BalancingRiderDrawing.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Test the BalancingRider drawing (2D)
+
+@author: Christoph M. Konrad
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from cyclistsocialforce.scenario import Scenario
+from cyclistsocialforce.vehicle import BalancingRiderBicycle
+
+class ParcoursScenario(Scenario):
+    """ A custom scenario of a cyclist following a parcours.
+    """
+    
+    def __init__(self, **kwargs):
+        
+        # set up a figure
+        self._make_figure()
+    
+        # create a bike and att ot the intersection.
+        self.bike = BalancingRiderBicycle(
+            (0, 0, 0, 5, 0, 0, 0, 0), id="BalancingRiderBike", saveForces=True
+        )
+        self.bike.params.v_desired_default = 4.0
+        destx = [0, 10, 0, 5, 10, 20, 21, 22, 23]
+        desty = [10, 20, 30, 40, 40, 40, 40, 40, 40]
+        
+        self.bike.setDestinations(destx, desty)
+
+        # call super constructor
+        kwargs['axes'] = self.ax
+        super().__init__(self._step_func, **kwargs)
+
+    def _make_figure(self):
+        self.xrange = np.array((-5, 5))
+        self.yrange = np.array((-5, 5))
+        self.fig, self.ax = plt.subplots(1,1, layout='constrained')
+        self.fig.set_size_inches(9,10)
+        self.ax.set_aspect("equal")
+        self.ax.set_xlim(self.xrange)
+        self.ax.set_ylim(self.yrange)
+        self.ax.set_ylabel("y [m]")
+        self.ax.set_xlabel("x [m]")
+        self.ax.set_title("Test: BalancingRiderDrawing (2D)")
+    
+    def _step_func(self):
+        """
+        Step function for this scenario.
+        """
+        if self.bike.drawing is None:
+            self.bike.add_drawing(self.ax, dest_marker_color_cur='red')
+        Fx, Fy = self.bike.calcDestinationForce()
+        self.bike.step(Fx, Fy)
+        self.ax.set_xlim(self.bike.s[0] + self.xrange)
+        self.ax.set_ylim(self.bike.s[1] + self.yrange)
+    
+def main():
+    t_end = 15
+    scn = ParcoursScenario(animate=True)
+    scn.run(t_end)
+    scn.bike.drawing.set_animated(False)
+
+if __name__=="__main__":
+    main()
